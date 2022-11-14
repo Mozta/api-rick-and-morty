58d943de0b0835c75800b951115c07474c500604
Rafael <perezaguirrerafael@gmail.com>
2022-11-14 09:59:10 -0600

    characters cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Rick and Morty</h1>
+        <h1 className="text-info my-4">Rick and Morty</h1>
 
         <div className="container">
           <Characters/>
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,9 +2,13 @@ import React from "react";
 const Character = ({ character }) => {
   return (
     <>
-      <div className="col-md-4">
-        <h4>{character.name}</h4>
-        <img src={character.image} alt="" />
+      <div className="col-md-4 mb-4">
+        <div className="card">
+          <img className="card-img-top" src={character.image} alt="" />
+          <div className="card-body">
+            <h4>{character.name}</h4>
+          </div>
+        </div>
       </div>
     </>
   );
