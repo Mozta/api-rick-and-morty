c5550fb3569f4aca364b979b773b6771d1fb26e8
Rafael <perezaguirrerafael@gmail.com>
2022-11-14 13:56:57 -0600

    update

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,7 +3,7 @@ const Character = ({ character }) => {
   return (
     <>
       <div className="col-md-4 mb-4">
-        <div className="card">
+        <div className="card me-5">
           <img className="card-img-top" src={character.image} alt="" />
           <div className="card-body">
             <h4>{character.name}</h4>
